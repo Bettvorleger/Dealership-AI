574d75bf123ec3846e10d8d122793584f39a03d0
Daniel Werner <dw86misy@studserv.uni-leipzig.de>
2022-07-21 21:38:10 +0200

    Changed root path for kubernetes proxy

--- services/backend/main.py
+++ services/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise import Tortoise
 
@@ -12,7 +13,7 @@ Tortoise.init_models(["src.database.models"], "models")
 from src.routes import users, notes
 
 subpath = '/fastapi'
-app = FastAPI(docs_url=f'{subpath}/docs', openapi_url=f'{subpath}/openapi.json')
+app = FastAPI(root_path=subpath, docs_url=f'{subpath}/docs')
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +25,21 @@ app.add_middleware(
 app.include_router(users.router, prefix=subpath)
 app.include_router(notes.router, prefix=subpath)
 
-print(TORTOISE_ORM)
-
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get(f'{subpath}/')
 def home():
-    return "Hello, World!"
+    return "Hello, World! TEST"
+
+@app.get(f'{subpath}/openapi.json', include_in_schema=False)
+async def access_openapi():
+    openapi = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+        tags=app.openapi_tags,
+    )
+    openapi["servers"] = [{"url": app.root_path}]
+    return openapi
